782a4d6b89cbc4ac42bc124e7b75de4dc4dc6773
Ian Grant Jeffries <ian@housejeffries.com>
2013-07-27 20:59:34 -0400

    Extract out method.

--- xiaoping/posts.py
+++ xiaoping/posts.py
@@ -1,24 +1,14 @@
 import json
 
-import requests
-import hawk
-
 
 class PostUtility:
 
     def get_posts_list(self):
         servers_list = self.discovery_response['post']['content']['servers']
         url = servers_list[0]['urls']['posts_feed']
-        credentials = {'id': self.id_value,
-                       'key': self.hawk_key,
-                       'algorithm': 'sha256'}
-        options = {'credentials': credentials,
-                   'app': self.app_id,
-                   'ext': ''}
-        header = hawk.client.header(url, 'GET', options=options)['field']
-        headers = {'Accept': 'application/vnd.tent.posts-feed.v0+json',
-                   'Authorization': header}
-        response = requests.get(url, headers=headers)
+        headers = {'Accept': 'application/vnd.tent.posts-feed.v0+json'}
+        method = 'GET'
+        response = self.make_request(url, method, headers=headers)
         attachment_dict = json.loads(response.text)
         return attachment_dict['posts']
 
@@ -28,22 +18,10 @@ class PostUtility:
     def post_status(self, text):
         servers_list = self.discovery_response['post']['content']['servers']
         url = servers_list[0]['urls']['new_post']
+        method = 'POST'
+        post_type = 'https://tent.io/types/status/v0#'
         content_type = 'application/vnd.tent.post.v0+json'
-        status_type = 'https://tent.io/types/status/v0#'
-        data_dict = {'type': status_type,
-                     'content': {'text': text}}
+        headers = {'Content-Type': content_type + '; type="' + post_type + '"'}
+        data_dict = {'type': post_type, 'content': {'text': text}}
         data = json.dumps(data_dict)
-        credentials = {'id': self.id_value,
-                       'key': self.hawk_key,
-                       'algorithm': 'sha256'}
-        options = {'credentials': credentials,
-                   'payload': data,
-                   'contentType': content_type,
-                   'app': self.app_id,
-                   'ext': '',
-                   'dlg': ''}
-        header = hawk.client.header(url, 'POST', options=options)['field']
-        headers = {'Authorization': header,
-                   'Content-Type': content_type + ';'
-                   + ' type="' + status_type + '"'}
-        return requests.post(url, data=data, headers=headers)
+        return self.make_request(url, method, headers=headers, data=data)
--- xiaoping/tentapp.py
+++ xiaoping/tentapp.py
@@ -1,3 +1,6 @@
+import requests
+import hawk
+
 from registration import RegistrationHelper
 from posts import PostUtility
 
@@ -21,3 +24,23 @@ class TentApp(RegistrationHelper, PostUtility):
         self.id_value = None
         self.hawk_key = None
         self.app_id = None
+
+    # A wrapper around PyHawk's header creator and requests.get/requests.post
+    # to make them easy to use within Xiaoping.
+    def make_request(self, url, method, headers=[], data=None):
+        credentials = {'id': self.id_value,
+                       'key': self.hawk_key,
+                       'algorithm': 'sha256'}
+        options = {'credentials': credentials,
+                   'app': self.app_id,
+                   'ext': ''}
+        if 'payload' in options:
+            options['payload'] = data
+            options['contentType'] = content_type
+            options['dlg'] = ''
+        header = hawk.client.header(url, method, options=options)['field']
+        headers['Authorization'] = header
+        if method.upper() == 'GET':
+            return requests.get(url, headers=headers)
+        if method.upper() == 'POST':
+            return requests.post(url, data=data, headers=headers)
