b8c7ad5a2b23ac05a335845d09cb2953d528a72d
Ian Grant Jeffries <ian@housejeffries.com>
2013-08-05 15:30:06 -0400

    Add simple parsing of header strings for tests.

--- xiaoping/tests/test_pyhawk.py
+++ xiaoping/tests/test_pyhawk.py
@@ -9,58 +9,47 @@ from hawk.hcrypto import calculate_payload_hash
 import pyhawk_monkeypatch
 
 
+# Test against examples of header creation from here:
+# https://tent.io/docs/authentication
+#
 # Note that this doesn't test time stamp, nonce, or hash creation.
 class TestTentDocExamples(unittest.TestCase):
 
     def setUp(self):
+        # Shared test data
         self.key = 'HX9QcbD-r3ItFEnRcAuOSg'
         self.url = 'https://example.com/posts'
         self.http_method = 'POST'
 
     def test_app_request_with_hash(self):
-        # Test data
+        # More test data
         self.hawk_id = 'exqbZWtykFZIh2D7cXi9dA'
         self.time_stamp = 1368996800
         self.nonce = '3yuYCD4Z'
         self.attachment_hash = 'neQFHgYKl/jFqDINrC21uLS0gkFglTz789rzcSr7HYU='
         self.app_id = 'wn6yzHGe5TLaT-fvOPbAyQ'
-
         # Answers
-        self.correct_header = ('Hawk id="exqbZWtykFZIh2D7cXi9dA", mac="2sttHC'
-                               'QJG9ejj1x7eCi35FP23Miu9VtlaUgwk68DTpM=", ts="'
-                               '1368996800", nonce="3yuYCD4Z", hash="neQFHgYK'
-                               'l/jFqDINrC21uLS0gkFglTz789rzcSr7HYU=", app="w'
-                               'n6yzHGe5TLaT-fvOPbAyQ"')
-        self.other_order = ('Hawk id="exqbZWtykFZIh2D7cXi9dA", ts="1368996800'
-                            '", nonce="3yuYCD4Z", hash="neQFHgYKl/jFqDINrC21u'
-                            'LS0gkFglTz789rzcSr7HYU=", mac="2sttHCQJG9ejj1x7e'
-                            'Ci35FP23Miu9VtlaUgwk68DTpM=", app="wn6yzHGe5TLaT'
-                            '-fvOPbAyQ"')
-
-        self.various_orders = [self.correct_header, self.other_order]
-
+        self.correct = ['id="exqbZWtykFZIh2D7cXi9dA"',
+                        'mac="2sttHCQJG9ejj1x7eCi35FP23Miu9VtlaUgwk68DTpM="',
+                        'ts="1368996800"',
+                        'nonce="3yuYCD4Z"',
+                        'hash="neQFHgYKl/jFqDINrC21uLS0gkFglTz789rzcSr7HYU="',
+                        'app="wn6yzHGe5TLaT-fvOPbAyQ"']
         # Run code
-        created_header = self.get_header()
-        self.assertIn(created_header, self.various_orders)
+        self.assertTrue(self.check_header(self.get_header(), self.correct))
 
     def test_relationship_request(self):
-        # Test data
+        # More test data
         self.hawk_id = 'exqbZWtykFZIh2D7cXi9dA'
         self.time_stamp = 1368996800
         self.nonce = '3yuYCD4Z'
-
         # Answers
-        self.correct_header = ('Hawk id="exqbZWtykFZIh2D7cXi9dA", mac="OO2ldB'
-                               'DSw8KmNHlEdTC4BciIl8+uiuCRvCnJ9KkcR3Y=", ts="'
-                               '1368996800", nonce="3yuYCD4Z"')
-        self.other_order = ('Hawk id="exqbZWtykFZIh2D7cXi9dA", ts="1368996800'
-                            '", nonce="3yuYCD4Z", mac="OO2ldBDSw8KmNHlEdTC4Bc'
-                            'iIl8+uiuCRvCnJ9KkcR3Y="')
-        self.various_orders = [self.correct_header, self.other_order]
-
+        self.correct = ['id="exqbZWtykFZIh2D7cXi9dA"',
+                        'mac="OO2ldBDSw8KmNHlEdTC4BciIl8+uiuCRvCnJ9KkcR3Y="',
+                        'ts="1368996800"',
+                        'nonce="3yuYCD4Z"']
         # Run code
-        created_header = self.get_header()
-        self.assertIn(created_header, self.various_orders)
+        self.assertTrue(self.check_header(self.get_header(), self.correct))
 
     def get_header(self):
         url = self.url
@@ -77,6 +66,29 @@ class TestTentDocExamples(unittest.TestCase):
         if hasattr(self, 'app_id'):
             options['app'] = self.app_id
         return header(url, method, options=options)['field']
+
+    # Takes a header string and a list of the contents that should make it up.
+    #
+    # Parses the header by removing each item in the contents list one at a
+    # time. If the header doesn't include one of the items or if it has
+    # leftover content remaining at the end besides spaces and commas,
+    # the header and correct_contents don't match.
+    def check_header(self, header, correct_contents):
+        if header.startswith('Hawk '):
+            header = header[len('Hawk '):]
+        else:
+            return False
+        for i in correct_contents:
+            if i in header:
+                header = header.replace(i, '', 1)
+            else:
+                return False
+        header = header.replace(' ', '')
+        header = header.replace(',', '')
+        if header != '':
+            return False
+        else:
+            return True
 
 if __name__ == '__main__':
     unittest.main()
