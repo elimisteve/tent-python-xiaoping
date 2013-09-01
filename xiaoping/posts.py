52d0916e976ab123a514179fa85eec8bea50c063
Ian Grant Jeffries <ian@housejeffries.com>
2013-08-31 23:38:43 -0400

    Add mentions to status posts.

--- xiaoping/posts.py
+++ xiaoping/posts.py
@@ -13,13 +13,18 @@ class PostUtility:
 
     # Create a status post on the server.
     #
-    # Returns the response for testing purposes.
-    def post_status(self, text):
+    # Returns the response.
+    def post_status(self, text, mentions_list=[]):
         url = self.get_server()['urls']['new_post']
         method = 'POST'
         post_type = 'https://tent.io/types/status/v0#'
         content_type = 'application/vnd.tent.post.v0+json'
         headers = {'Content-Type': content_type + '; type="' + post_type + '"'}
-        data_dict = {'type': post_type, 'content': {'text': text}}
+        mentions = []
+        for entity in mentions_list:
+            mentions.append({'entity': entity})
+        data_dict = {'type': post_type,
+                     'content': {'text': text},
+                     'mentions': mentions}
         data = json.dumps(data_dict)
         return self.make_request(url, method, headers, data)
