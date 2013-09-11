95a509db6f16bee9a48bbff48fb1f3b2d17fd079
Ian Grant Jeffries <ian@housejeffries.com>
2013-09-11 15:14:55 -0400

    Add setup file.

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+setup(
+    name='xiaoping',
+    version='0.1.0',
+    author='Ian Grant Jeffries',
+    author_email='ian@housejeffries.com',
+    url='https://github.com/seagreen/tent-python-xiaoping',
+    license=open('MIT-LICENSE.txt').read(),
+    packages=['xiaoping']
+)
